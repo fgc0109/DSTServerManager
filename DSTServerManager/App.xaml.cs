277c235b3e612c71ebe32ba185b9f6705ecea600
Chasel <fgc0109@126.com>
2016-12-04 15:59:12 +0800

    没有时间处理所有异常情况,先添加一个全局异常捕获

--- DSTServerManager/App.xaml.cs
+++ DSTServerManager/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace DSTServerManager
 {
@@ -13,5 +14,34 @@ namespace DSTServerManager
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+
+        }
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            Application.Current.DispatcherUnhandledException += Application_DispatcherUnhandledException;
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs exception)
+        {
+            string errorCaption = "意外的操作";
+            string errorInfoTip = "我们很抱歉,当前应用程序遇到一些问题..\r\n";
+            MessageBox.Show(errorInfoTip + exception.Exception, errorCaption, MessageBoxButton.OK, MessageBoxImage.Information);
+            exception.Handled = true;
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs exception)
+        {
+            string errorCaption = "意外的操作";
+            string errorInfoTip = "我们很抱歉,当前应用程序遇到一些问题,请联系管理员..\r\n";
+            MessageBox.Show(errorInfoTip + exception.ExceptionObject, errorCaption, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Application_SessionEnding(object sender, SessionEndingCancelEventArgs e)
+        {
+
+        }
     }
 }
