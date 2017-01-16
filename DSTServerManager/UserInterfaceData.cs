add6cc3ace5c4807de5067a061d36299f9dd8713
Chasel <fgc0109@126.com>
2017-01-16 20:46:30 +0800

    [DSTServerManager] 提高了项目的.net版本,提高了sqlite的版本,增加了界面需要的列内容

--- DSTServerManager/UserInterfaceData.cs
+++ DSTServerManager/UserInterfaceData.cs
@@ -532,6 +532,8 @@ namespace DSTServerManager
             for (int i = 0; i < columns; i++)
                 clusterServersTable.Columns.Add("Column_" + i.ToString());
 
+            modification.Columns.Add("ModInfo_Adde");
+            modification.Columns.Add("ModInfo_Actv");
             modification.Columns.Add("ModInfo_Indx");
             modification.Columns.Add("ModInfo_Name");
             modification.Columns.Add("ModInfo_Auth");
